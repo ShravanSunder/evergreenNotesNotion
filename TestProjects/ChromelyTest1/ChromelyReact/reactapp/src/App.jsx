a9d61afdb2ffe42516ae646abc944ed88ca3225a
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2020-05-06 10:21:02 -0400

    understood react scripts

--- TestProjects/ChromelyTest1/ChromelyReact/reactapp/src/App.jsx
+++ TestProjects/ChromelyTest1/ChromelyReact/reactapp/src/App.jsx
@@ -5,32 +5,33 @@ import './App.css';
 import { openExternalUrl } from './services/Chromely.Service.js'; 
 
 export default class App extends Component {
-  constructor(props) {
+    constructor(props) {
     super(props);
 
     this.showDevTools = this.showDevTools.bind(this);
-  }
+    }
 
 
-  render() {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path='/' component={Home} />
-                    <Route path="*" component={Home} />
-                </Switch>
-            </div>
-            <div className="dropdown-item">
-                <Link to={'/showDevTools'} className="nav-link" onClick={this.showDevTools}>Show DevTools Remote</Link>
-                <Link to={'/showDevTools'} className="nav-link" onClick={this.showDevTools}>Show DevTools Remote</Link>
-            </div>
-        </Router>
-    );
-  }
-}
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path="*" component={Home} />
+                    </Switch>
+                </div>
+                <div className="dropdown-item">
+                    <Link to={'/showDevTools'} className="nav-link" onClick={this.showDevTools}>Show DevTools Remote</Link>
+                    <Link to={'/showDevTools'} className="nav-link" onClick={this.showDevTools}>Show DevTools Remote</Link>
+                </div>
+            </Router>
+        );
+    }
 
-showDevTools(event) {
-    event.preventDefault();
-    openExternalUrl("http://command.com/democontroller/showdevtools");
+
+    showDevTools(event) {
+        event.preventDefault();
+        openExternalUrl("http://command.com/democontroller/showdevtools");
+    }
 }
