ae38fc86b54f43ca67553a9c4c63d1663db09c01
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2020-05-08 09:50:57 -0400

    #6jb4c5 parcel & react was working.  the get document by id needs id!  doh

--- TestProjects/ChromelyTest1/ChromelyReact/reactapp/src/App.tsx
+++ TestProjects/ChromelyTest1/ChromelyReact/reactapp/src/App.tsx
@@ -2,14 +2,15 @@ import './App.css';
 import 'typeface-roboto'
 
 //react
-import React, { Component } from 'react';
+import React, { Component, SyntheticEvent } from 'react';
 //react-router
-import { BrowserRouter as Router, Switch, Route, Link, Redirection, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
 //pages
 import { openExternalUrl } from './services/Chromely.Service.js'; 
 import Home from './components/Home';
 import About from './components/About';
+
 
 
 
@@ -22,9 +23,15 @@ const App: React.FC = () => (
           </Switch>
       </div>
       <div className="dropdown-item">
-          <Link to={'/showDevTools'} className="nav-link" onClick={this.showDevTools}>Show DevTools Remote</Link>
+          <Link to={'/showDevTools'} className="nav-link" onClick={showDevTools}>Show DevTools Remote</Link>
       </div>
   </Router>
 );
+
+
+export function showDevTools(event: SyntheticEvent) {
+    event.preventDefault();
+    openExternalUrl("http://command.com/democontroller/showdevtools");
+}
 
 export default App;
--- TestProjects/ChromelyTest1/ChromelyReact/reactapp/src/index.js
+++ TestProjects/ChromelyTest1/ChromelyReact/reactapp/src/index.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '~/App';
+import * as serviceWorker from '~serviceWorker';
 
-render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
