1d7507fbce1644da5a83db1317699b6c607e9caa
Shravan S <Shravan.retail@gmail.com>
2020-05-08 20:17:31 -0400

    #6jb4b2 got the spotify c# library.  also got polly lib retries and fualt handling and flutr lib for res

--- TestProjects/ChromelyTest1/ChromelyReact/reactapp/.storybook/main.js
+++ TestProjects/ChromelyTest1/ChromelyReact/reactapp/.storybook/main.js
@@ -1,8 +1,12 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js', '../src/**/*.stories.jsx', '../src/**/*.stories.tsx', '../src/**/*.stories.ts'],
+  stories: ['../src/**/*.stories.(js|mdx|jsx|tsx|ts)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-storysource',
+    '@storybook/addon-docs'
   ],
 };
