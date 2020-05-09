e6d3a02bf6d52697ad396350f101d87802ea13a9
Shravan S <Shravan.retail@gmail.com>
2020-05-09 17:48:28 -0400

    #6uhqh2  careless package installation was the case.  now everything in storybook is working

--- TestProjects/ChromelyTest1/ChromelyReact/reactapp/.storybook/main.js
+++ TestProjects/ChromelyTest1/ChromelyReact/reactapp/.storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx|jsx|tsx|ts)'],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-docs',
+ //   '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-docs/register',
     '@storybook/addon-viewport/register',
-    '@storybook/addon-storysource',
-    '@storybook/addon-docs'
+    '@storybook/addon-storysource/register'
   ],
 };
--- TestProjects/ChromelyTest1/ChromelyReact/reactapp/jest.config.js
+++ TestProjects/ChromelyTest1/ChromelyReact/reactapp/jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx", "node"],
+  "transform": {
+   // "^.+\\.[tj]sx?$": "babel-jest",
+    "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx"
+  }
 };
