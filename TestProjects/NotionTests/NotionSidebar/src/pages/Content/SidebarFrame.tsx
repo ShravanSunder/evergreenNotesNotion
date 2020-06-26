7a9c7bb8b77dabe55261cdcb95b895d93c751744
Shravan S <Shravan.retail@gmail.com>
2020-06-26 18:06:55 -0400

    #7 working on the sidebar sizeing.  NOTE: devserver broken. perhaps never worked.  logs were from another extension

--- TestProjects/NotionTests/NotionSidebar/src/pages/Content/SidebarFrame.tsx
+++ TestProjects/NotionTests/NotionSidebar/src/pages/Content/SidebarFrame.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export const mountSidebar = (
-   sidebar: HTMLElement,
-   newRoot: HTMLElement,
-   notionApp: HTMLElement
-) => {
+export const mountSidebar = (sidebar: HTMLElement) => {
    console.log('render');
-   newRoot.append(notionApp);
-   newRoot.append(sidebar);
 
    ReactDOM.render(<Sidebar />, sidebar);
 };
--- TestProjects/NotionTests/NotionSidebar/src/pages/Content/index.ts
+++ TestProjects/NotionTests/NotionSidebar/src/pages/Content/index.ts
@@ -3,7 +3,7 @@ import { mountSidebar } from './SidebarFrame';
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
    if (request.command === 'notionTabLoaded') {
       initalize();
    } else if (request.command === 'chromeOnClick') {
@@ -14,26 +14,46 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 const notionAppId = 'notion-app';
+const notionFrameClass = 'notion-frame';
+const notionSideClass = 'notion-sidebar-container';
 const notionSidebarRootId = 'notion-sidebar-root-987384';
 const notionBaseNewRootId = 'notion-base-new-root';
+var initalized = false;
 const initalize = () => {
-   if (!document.getElementById(notionSidebarRootId)) {
-      let sidebarRoot = document.createElement('div');
-      document.body.appendChild(sidebarRoot);
-      sidebarRoot.setAttribute('id', notionSidebarRootId);
+   if (!initalized) {
+      initalized = true;
+      if (!document.getElementById(notionSidebarRootId)) {
+         let sidebarRoot = document.createElement('div');
+         document.body.appendChild(sidebarRoot);
+         sidebarRoot.setAttribute('id', notionSidebarRootId);
 
-      let newRoot = document.createElement('div');
-      document.body.appendChild(newRoot);
-      newRoot.setAttribute('id', notionBaseNewRootId);
+         let newRoot = document.createElement('div');
+         document.body.appendChild(newRoot);
+         newRoot.setAttribute('id', notionBaseNewRootId);
+      }
+
+      let sidebarRoot = document.getElementById(notionSidebarRootId);
+      let notionApp = document.getElementById(notionAppId);
+      let notionFrame = document.getElementsByClassName(notionFrameClass)[0];
+      let notionSide = document.getElementsByClassName(notionSideClass)[0];
+
+      if (sidebarRoot && notionApp && notionFrame) {
+         let nSideWidth = Math.round(notionSide.getBoundingClientRect().width);
+         let fullWidth = Math.round(notionFrame.getBoundingClientRect().width);
+         let sidebarWidth = Math.round((fullWidth - nSideWidth) * 0.2);
+         let nFrameWidth = Math.round((fullWidth - nSideWidth) * 0.8);
+         let nAppWidth = fullWidth - sidebarWidth;
+
+         //Todo add a obeserver: https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+         document.append(sidebarRoot);
+         notionFrame.setAttribute('style', `max-width:${nFrameWidth}px`);
+         notionApp.setAttribute('style', `max-width:${nAppWidth}px`);
+         sidebarRoot.setAttribute('style', `max-width:${sidebarWidth}px`);
+      }
    }
 
-   var sidebarRoot = document.getElementById(notionSidebarRootId);
-   var newRoot = document.getElementById(notionBaseNewRootId);
-   var notionApp = document.getElementById(notionAppId);
-
-   if (sidebarRoot && newRoot && notionApp) {
-      mountSidebar(sidebarRoot, newRoot, notionApp);
-   }
+   let sidebarRoot = document.getElementById(notionSidebarRootId);
+   sidebarRoot && mountSidebar(sidebarRoot);
 };
 
 const toggleSidebar = () => {};
