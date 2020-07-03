541db021e7ea8dc423804e45637b8365148d8abf
Shravan S <Shravan.retail@gmail.com>
2020-07-03 00:40:57 -0400

    #14 #13 super agetn works with this!

--- TestProjects/NotionTests/NotionSidebar/src/pages/Sidebar/Sidebar.tsx
+++ TestProjects/NotionTests/NotionSidebar/src/pages/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { msgTypes } from '../Common/msgTypes';
+import superagent from 'superagent';
 
 ///////////////
 
@@ -15,9 +16,40 @@ export const App = () => {
    );
 };
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function(
+   request,
+   sender,
+   sendResponse
+) {
    if (request.msgType === msgTypes.cookies) {
       console.log('received cookies', request.notionCookies);
+      var data = await superagent
+         .post('https://www.notion.so/api/v3/search')
+         .set(
+            'x-notion-active-user-header',
+            '79888584-c56f-4b10-9e82-9411c5793cfa'
+         )
+         .set('Cookie', request.notionCookies)
+         .send({
+            type: 'BlocksInSpace',
+            query: 'search',
+            spaceId: '8c4bb92b-8b88-4caa-9168-93ebe20f619c',
+            limit: 20,
+            filters: {
+               isDeletedOnly: false,
+               excludeTemplates: false,
+               isNavigableOnly: false,
+               requireEditPermissions: false,
+               ancestors: [],
+               createdBy: [],
+               editedBy: [],
+               lastEditedTime: {},
+               createdTime: {},
+            },
+            sort: 'Relevance',
+            source: 'quick_find',
+         });
+      console.log(data);
    }
 });
 
