80847cc63b1cd112e2813deb49719cf9a3f52552
Shravan Sunder <shravan.retail@gmail.com>
2020-07-30 11:35:20 -0400

    removed old code

--- TestProjects/UdemyNodeJs/js-only.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-  return num1 + num2;
-}
-button.addEventListener("click", function() {
-  console.log(add(input1.value, input2.value));
-});
