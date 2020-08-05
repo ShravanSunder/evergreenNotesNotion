d154a1cb3998b046c2786dba749c9e7dd44f7875
Shravan Sunder <shravan.retail@gmail.com>
2020-08-05 01:43:15 -0400

    #47 #48 simple top bar for search and highlights

--- NotionSidecar/src/notion/components/Layout.tsx
+++ NotionSidecar/src/notion/components/Layout.tsx
@@ -1,5 +1,5 @@
 //import { hot } from 'react-hot-loader/root';
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import {
    cookieSelector,
@@ -17,11 +17,41 @@ import { thunkStatus } from 'aNotion/types/thunkStatus';
 import { notionSiteActions } from './notionSiteSlice';
 import { getCurrentUrl } from 'aCommon/extensionHelpers';
 import { AppPromiseDispatch } from 'aNotion/providers/reduxStore';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+
+import { FindInPageTwoTone, BookTwoTone } from '@material-ui/icons/';
+import { lightGreen } from '@material-ui/core/colors';
+import { makeStyles, Theme, createStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+   createStyles({
+      grouped: {
+         margin: theme.spacing(0.5),
+         border: 'none',
+         '&:not(:first-child)': {
+            borderRadius: theme.shape.borderRadius,
+         },
+         '&:first-child': {
+            borderRadius: theme.shape.borderRadius,
+         },
+      },
+      toggleButton: {
+         color: lightGreen[700],
+         backgroundColor: lightGreen[50],
+         '&$checked': {
+            color: lightGreen[900],
+            backgroundColor: lightGreen[200],
+         },
+      },
+   })
+);
 
 const Layout = () => {
    const dispatch: AppPromiseDispatch<any> = useDispatch();
    const cookie = useSelector(cookieSelector, shallowEqual);
    const navigation = useSelector(navigationSelector, shallowEqual);
+
+   const classes = useStyles();
 
    const updateCurrentPageId = useCallback(async () => {
       let url = await getCurrentUrl();
@@ -49,9 +79,37 @@ const Layout = () => {
       return () => {};
    }, [navigation.pageId, navigation.url, dispatch]);
 
+   const [tab, setTab] = useState('references');
+
+   const handleTab = (
+      event: React.MouseEvent<HTMLElement>,
+      newTab: string | null
+   ) => {
+      if (newTab != null) setTab(newTab);
+   };
+
    return (
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-         <ReferencesPane />
+         <React.Fragment>
+            <ToggleButtonGroup
+               className={classes.grouped}
+               size="small"
+               value={tab}
+               exclusive
+               onChange={handleTab}>
+               <ToggleButton
+                  value="references"
+                  className={classes.toggleButton}>
+                  <BookTwoTone></BookTwoTone>
+               </ToggleButton>
+               <ToggleButton value="search" className={classes.toggleButton}>
+                  <FindInPageTwoTone></FindInPageTwoTone>
+               </ToggleButton>
+               <ToggleButton></ToggleButton>
+               <ToggleButton></ToggleButton>
+            </ToggleButtonGroup>
+            <ReferencesPane />
+         </React.Fragment>
       </ErrorBoundary>
    );
 };
