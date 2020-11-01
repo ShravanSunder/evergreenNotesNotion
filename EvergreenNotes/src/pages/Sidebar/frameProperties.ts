119902bcc0ce53731a75d88db9eb7fc055bdd7fe
Shravan Sunder <shravan.retail@gmail.com>
2020-11-01 15:57:59 -0500

    #81 changed the scroll bar

--- EvergreenNotes/src/pages/Sidebar/frameProperties.ts
+++ EvergreenNotes/src/pages/Sidebar/frameProperties.ts
@@ -1,6 +1,6 @@
 export const appPositionTop = () => 43;
 export const appWidth = (wWidth: number) => Math.max(wWidth * 0.25, 290);
 export const appPositionLeft = (wWidth: number) =>
-   wWidth - appWidth(wWidth) - 21;
+   wWidth - appWidth(wWidth) - 18;
 export const appHeight = (wHeight: number) =>
    Math.max(wHeight - appPositionTop() - 1, 400);
